b1df041df66ac3fcc5792beba6885766b8a44aad
Vasyl-Hnatyshyn <gnatv7077@gmail.com>
2020-04-27 12:05:37 +0300

    Updates

---
+++ precache-manifest.427a4e9aa959b2bf2403e13608ef64bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cde75b9f359c384872f",
+    "url": "/ToDo-List-React-Hooks/static/css/main.a70008c8.chunk.css"
+  },
+  {
+    "revision": "0cde75b9f359c384872f",
+    "url": "/ToDo-List-React-Hooks/static/js/main.960d6c7b.chunk.js"
+  },
+  {
+    "revision": "3b8d148cc49bef543be8",
+    "url": "/ToDo-List-React-Hooks/static/js/runtime~main.efea75da.js"
+  },
+  {
+    "revision": "e66ac356fa173e38097b",
+    "url": "/ToDo-List-React-Hooks/static/css/2.4356eb83.chunk.css"
+  },
+  {
+    "revision": "e66ac356fa173e38097b",
+    "url": "/ToDo-List-React-Hooks/static/js/2.d06e9420.chunk.js"
+  },
+  {
+    "revision": "0a6db1bcc819071fa76af2d68ade1313",
+    "url": "/ToDo-List-React-Hooks/index.html"
+  }
+];
